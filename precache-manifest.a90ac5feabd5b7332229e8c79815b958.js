44ebf38d504549352da845278cc4a9c76e647142
Sahil Singh <sahilsinghmca@gmail.com>
2020-07-12 13:45:16 +0530

    Updates

---
+++ precache-manifest.a90ac5feabd5b7332229e8c79815b958.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35c68c9a7706b3fe9eb6c1135b5313aa",
+    "url": "/connect-four/index.html"
+  },
+  {
+    "revision": "59ea4f3cc8930ba5d591",
+    "url": "/connect-four/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "85cbad5c0c0fa0e6730a",
+    "url": "/connect-four/static/css/main.5e0e9c1e.chunk.css"
+  },
+  {
+    "revision": "59ea4f3cc8930ba5d591",
+    "url": "/connect-four/static/js/2.65455b7a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/connect-four/static/js/2.65455b7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85cbad5c0c0fa0e6730a",
+    "url": "/connect-four/static/js/main.f3e6eacb.chunk.js"
+  },
+  {
+    "revision": "b9b26852b063a15b011d",
+    "url": "/connect-four/static/js/runtime-main.28281732.js"
+  },
+  {
+    "revision": "d10b9ad2f219f337692b94d38fe6f701",
+    "url": "/connect-four/static/media/4inarow.d10b9ad2.png"
+  },
+  {
+    "revision": "42cf9f0820d16f3ac2c26a7710ce70f2",
+    "url": "/connect-four/static/media/Poppins-Black.42cf9f08.ttf"
+  },
+  {
+    "revision": "c23534acbeddbaadfd0ab2d2bbfdfc84",
+    "url": "/connect-four/static/media/Poppins-Bold.c23534ac.ttf"
+  },
+  {
+    "revision": "6b78c7ec468eb0e13c6c5c4c39203986",
+    "url": "/connect-four/static/media/Poppins-ExtraBold.6b78c7ec.ttf"
+  },
+  {
+    "revision": "8ea2657f263b6b2a404bfb57f4160013",
+    "url": "/connect-four/static/media/Poppins-ExtraLight.8ea2657f.ttf"
+  },
+  {
+    "revision": "2a47a29ceb33c966c8d79f8d5a5ea448",
+    "url": "/connect-four/static/media/Poppins-Light.2a47a29c.ttf"
+  },
+  {
+    "revision": "ba95810b56f476990ca71d15139d5111",
+    "url": "/connect-four/static/media/Poppins-Medium.ba95810b.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/connect-four/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "342ba3d8ac29ac8c38d7cef8efbf2dc9",
+    "url": "/connect-four/static/media/Poppins-SemiBold.342ba3d8.ttf"
+  },
+  {
+    "revision": "c0fafa8397437c95848724aed686d63b",
+    "url": "/connect-four/static/media/Poppins-Thin.c0fafa83.ttf"
+  }
+]);
